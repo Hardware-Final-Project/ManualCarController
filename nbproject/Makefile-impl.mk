dbba0a76ee6eee078929c1dbd595e013102b4dd3
JessYu-1011 <learningen13@gmail.com>
2025-12-15 22:21:31 +0800

    Add key

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=ManualCarController.X
+PROJECTNAME=ManualCarController
 
 # Active Configuration
 DEFAULTCONF=default
